477357dec10eec9c07f24308a2c8e423cc322be0
Cameron Doane <camdoane@uw.edu>
2015-11-15 22:29:50 -0800

    updated password strength meter

--- coordinatehw/js/app.js
+++ coordinatehw/js/app.js
@@ -17,31 +17,71 @@ angular.module("validationApp", [])
         };
     })
     .controller("signupForm", function($scope){
-        $scope.passwordStrength = "100%";
+        $scope.passwordStrength = "0";
         $scope.strengthClass = "progress-bar-danger";
         $scope.strengthWord = "weak";
 
-        $scope.getStrengthWord = function(){
-            /* move this out of ng-class */
-            var strength = $scope.getStrength();
-            if(strength < 25){
+        $scope.getStrengthWord = function(str){
+            if(str < 25){
                 $scope.strengthWord = "Weak";
-                return "progress-bar-danger"
+                $scope.strengthClass = "progress-bar-danger"
             }
-            else if(strength < 50){
+            else if(str < 50){
                 $scope.strengthWord = "Okay";
-                return "progress-bar-warning"
+                $scope.strengthClass = "progress-bar-warning"
             }
-            else if(strength < 75){
+            else if(str < 75){
                 $scope.strengthWord = "Strong";
-                return "progress-bar-info"
+                $scope.strengthClass = "progress-bar-info"
             }
-            else if(strength <= 100){
+            else {
                 $scope.strengthWord = "Really Strong!";
-                return "progress-bar-success"
+                $scope.strengthClass = "progress-bar-success"
             }
         };
         $scope.getStrength = function(){
-            return parseInt($scope.passwordStrength.replace("%", ""));
+            if($scope.signup && $scope.signup.password.length > 0){
+                var variety = [0,0,0,0];
+                var index = 0;
+                for(index; index < $scope.signup.password.length; index++){
+                    var currentChar = $scope.signup.password.charAt(index);
+                    if(containsSpecial(currentChar)){
+                        variety[3]++;
+                    }
+                    else if(containsNumber(currentChar)){
+                        variety[2]++;
+                    }
+                    else if(containsCapital(currentChar)){
+                        variety[1]++;
+                    }
+                    else {
+                        variety[0]++;
+                    }
+                }
+                console.log(variety);
+                index = 0;
+                var everything = 0;
+                for(index; index < variety.length; index++){
+                    if(variety[index] > 0){
+                        everything++;
+                    }
+                }
+                var multiplier = 5;
+                /* limit to the variety of the password */
+                var strength = Math.min(everything * 25, $scope.signup.password.length * multiplier);
+                console.log($scope.signup.password);
+                console.log(strength);
+                $scope.getStrengthWord(strength);
+                $scope.passwordStrength = strength;
+            }
         };
+        function containsSpecial(str){
+            return /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(str);
+        }
+        function containsNumber(str){
+            return /[0123456789]/g.test(str);
+        }
+        function containsCapital(str){
+            return /[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/g.test(str);
+        }
     });
