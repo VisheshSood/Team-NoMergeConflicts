3de06540975fb102738ab57873564b670a2a8129
Anya Hsu <anyahsu280@gmail.com>
2015-11-15 16:07:10 -0800

    added submit btn, clear btn, first/last name fields

--- coordinatehw/js/app.js
+++ coordinatehw/js/app.js
@@ -1,4 +1,16 @@
 angular.module("validationApp", [])
     .controller("signupForm", function($scope){
 
+        $scope.formSuccess = false;
+
+        $scope.resetForm = function() {
+            $scope.user = undefined;
+            $scope.formSuccess = false;
+        }
+
+        $scope.signUp = function() {
+            // TODO: form validation on user
+
+            $scope.formSuccess = true;
+        }
     });
