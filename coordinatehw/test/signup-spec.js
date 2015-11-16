95244e5b19e25307376dbf11efb4e3f2ac98ec6d
Anya Hsu <anyahsu280@gmail.com>
2015-11-15 19:46:20 -0800

    updated test

--- coordinatehw/test/signup-spec.js
+++ coordinatehw/test/signup-spec.js
@@ -4,4 +4,8 @@ describe("the signup page", function(){
     beforeEach(function(){
         browser.get("http://localhost:8000");
     });
+
+    it('must clear all input boxes', function() {
+        expect()
+    })
 });
