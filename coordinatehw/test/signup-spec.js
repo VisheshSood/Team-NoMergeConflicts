9487d8aa072d765c60222a68d401e388fa138235
Anya Hsu <anyahsu280@gmail.com>
2015-11-15 22:11:35 -0800

    tested last name validation, added error text

--- coordinatehw/test/signup-spec.js
+++ coordinatehw/test/signup-spec.js
@@ -1,11 +1,30 @@
 describe("the signup page", function(){
-    
+    var firstName = element(by.model("user.first_name"));
+    var lastName = element(by.model("user.last_name"));
+    var email = element(by.model("user.email"));
 
     beforeEach(function(){
         browser.get("http://localhost:8000");
     });
 
     it('must clear all input boxes', function() {
-        expect(element(by.id("resetButton"))).toEqual("");
-    })
+        firstName.sendKeys("Sam");
+        lastName.sendKeys("I am");
+        email.sendKeys("IDoNotLikeGreenEggsAndHam@gmail.com");
+        element(by.buttonText("Reset")).click();
+        expect(firstName.getText()).toEqual("");
+        expect(lastName.getText()).toEqual("");
+        expect(email.getText()).toEqual("");
+    });
+
+    it('must have valid last name', function() {
+        var requiredMsg = element(by.className("validation-error"));
+
+        expect(requiredMsg.isPresent()).toEqual(false);
+        lastName.sendKeys('abc');
+        lastName.clear();
+        expect(requiredMsg.isPresent()).toEqual(true);
+        lastName.sendKeys('abc');
+        expect(requiredMsg.isPresent()).toEqual(false);
+    });
 });
