7e5d366ff5c0cfa0d5ab02df02acb4f97149f405
Cameron Doane <camdoane@uw.edu>
2015-11-17 12:59:41 -0800

    fix duplicate unit test error

--- coordinatehw/test/signup-spec.js
+++ coordinatehw/test/signup-spec.js
@@ -63,48 +63,5 @@ describe("the signup page", function () {
         confirmPasswordInput.clear();
         confirmPasswordInput.sendKeys("password");
         expect(error.isPresent()).toEqual(false);
-
-    });
-
-    describe("password strength functions", function () {
-        beforeEach(module("validationApp"));
-        var $controller;
-        beforeEach(inject(function (_$controller_) {
-            $controller = _$controller_;
-        }));
-        function checkStrength(password, toCheck, expected) {
-            $scope.user.password = password;
-            $scope.getStrength();
-            expect(toCheck).toEqual(expected);
-        }
-
-        it("should have capped strength when there is no variety", function () {
-            var $scope = {};
-            var controller = $controller('userForm', {$scope: $scope});
-
-            checkStrength("thisisreallylongpasswordwithnovariety", $scope.passwordStrength, "25");
-            checkStrength("thisisreallylongpasswordwithnovarietyA", $scope.passwordStrength, "50");
-            checkStrength("thisisreallylongpasswordwithnovarietyA1", $scope.passwordStrength, "75");
-            checkStrength("thisisreallylongpasswordwithnovarietyA1!", $scope.passwordStrength, "100");
-        });
-        it("should increment by multiplier", function () {
-            /* maybe should be a global on scope, but don't like that */
-            var multiplier = 8;
-            checkStrength("a", $scope.passwordStrength, (multiplier).toString());
-            checkStrength("ab", $scope.passwordStrength, (multiplier * 2).toString());
-            checkStrength("abc", $scope.passwordStrength, (multiplier * 3).toString());
-        });
-        it("should give proper strength words", function () {
-            checkStrength("thisisreallylongpasswordwithnovariety", $scope.strengthWord, "Weak");
-            checkStrength("thisisreallylongpasswordwithnovarietyA", $scope.strengthWord, "Okay");
-            checkStrength("thisisreallylongpasswordwithnovarietyA1", $scope.strengthWord, "Strong");
-            checkStrength("thisisreallylongpasswordwithnovarietyA1!", $scope.strengthWord, "Really Strong!");
-        });
-        it("should give proper progress bar classes", function () {
-            checkStrength("thisisreallylongpasswordwithnovariety", $scope.strengthClass, "progress-bar-danger");
-            checkStrength("thisisreallylongpasswordwithnovarietyA", $scope.strengthClass, "progress-bar-warning");
-            checkStrength("thisisreallylongpasswordwithnovarietyA1", $scope.strengthClass, "progress-bar-info");
-            checkStrength("thisisreallylongpasswordwithnovarietyA1!", $scope.strengthClass, "progress-bar-success");
-        });
     });
 });
