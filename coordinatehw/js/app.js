8d9c41533ab74d08b166be74a36a850a1ba1c577
Anya Hsu <anyahsu280@gmail.com>
2015-11-16 22:25:20 -0800

    updated variables in test so things work

--- coordinatehw/js/app.js
+++ coordinatehw/js/app.js
@@ -1,62 +1,62 @@
 angular.module("validationApp", [])
-    .directive("compareTo", function(){
+    .directive("compareTo", function () {
         return {
             require: "ngModel",
             scope: {
                 otherModelValue: "=compareTo"
             },
-            link: function(scope, element, attributes, ngModel){
-                ngModel.$validators.compareTo = function(modelValue) {
+            link: function (scope, element, attributes, ngModel) {
+                ngModel.$validators.compareTo = function (modelValue) {
                     return modelValue === scope.otherModelValue;
                 };
 
-                scope.$watch("otherModelValue", function() {
+                scope.$watch("otherModelValue", function () {
                     ngModel.$validate();
                 });
             }
         };
     })
-    .controller("signupForm", function($scope){
+    .controller("signupForm", function ($scope) {
         $scope.passwordStrength = "0";
         $scope.strengthClass = "progress-bar-danger";
         $scope.strengthWord = "Weak";
         $scope.formSuccess = false;
 
-        $scope.resetForm = function() {
+        $scope.resetForm = function () {
             $scope.user = undefined;
             $scope.formSuccess = false;
         }
 
-        $scope.signUp = function() {
+        $scope.signUp = function () {
             // TODO: form validation on user
 
             $scope.formSuccess = true;
         }
 
-        $scope.getStrength = function(){
+        $scope.getStrength = function () {
             var strength = 0;
-            if($scope.signup && $scope.signup.password){
-                strength = getStrengthValue($scope.signup.password);
+            if ($scope.user && $scope.user.password) {
+                strength = getStrengthValue($scope.user.password);
             }
             $scope.passwordStrength = strength.toString();
             var data = getStrengthWord(strength);
             $scope.strengthWord = data.word;
             $scope.strengthClass = data.class;
         };
-        function getStrengthValue(word){
-            if(word.length > 0){
-                var variety = [0,0,0,0];
+        function getStrengthValue(word) {
+            if (word.length > 0) {
+                var variety = [0, 0, 0, 0];
                 var index = 0;
                 /* get the variety of the password */
-                for(index; index < word.length; index++){
+                for (index; index < word.length; index++) {
                     var currentChar = word.charAt(index);
-                    if(containsSpecial(currentChar)){
+                    if (containsSpecial(currentChar)) {
                         variety[3]++;
                     }
-                    else if(containsNumber(currentChar)){
+                    else if (containsNumber(currentChar)) {
                         variety[2]++;
                     }
-                    else if(containsCapital(currentChar)){
+                    else if (containsCapital(currentChar)) {
                         variety[1]++;
                     }
                     else {
@@ -66,8 +66,8 @@ angular.module("validationApp", [])
                 index = 0;
                 var everything = 0;
                 /* get the unique variety */
-                for(index; index < variety.length; index++){
-                    if(variety[index] > 0){
+                for (index; index < variety.length; index++) {
+                    if (variety[index] > 0) {
                         everything++;
                     }
                 }
@@ -78,27 +78,31 @@ angular.module("validationApp", [])
             }
             return 0;
         }
-        function getStrengthWord(str){
-            if(str <= 25){
+
+        function getStrengthWord(str) {
+            if (str <= 25) {
                 return {word: "Weak", class: "progress-bar-danger"};
             }
-            else if(str <= 50){
+            else if (str <= 50) {
                 return {word: "Okay", class: "progress-bar-warning"};
             }
-            else if(str <= 75){
-                return {word: "Strong", class:"progress-bar-info"}
+            else if (str <= 75) {
+                return {word: "Strong", class: "progress-bar-info"}
             }
             else {
-                return {word: "Really Strong!", class:"progress-bar-success"}
+                return {word: "Really Strong!", class: "progress-bar-success"}
             }
         }
-        function containsSpecial(str){
+
+        function containsSpecial(str) {
             return /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?\@]/g.test(str);
         }
-        function containsNumber(str){
+
+        function containsNumber(str) {
             return /[0123456789]/g.test(str);
         }
-        function containsCapital(str){
+
+        function containsCapital(str) {
             return /[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/g.test(str);
         }
     });
