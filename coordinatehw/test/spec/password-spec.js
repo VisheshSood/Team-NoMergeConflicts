057b88f55d8c04c217afc57f7e59b5144a958489
Cameron Doane <camdoane@uw.edu>
2015-11-16 21:32:30 -0800

    fixes #23 and finalizes testing password

--- coordinatehw/test/spec/password-spec.js
+++ coordinatehw/test/spec/password-spec.js
@@ -1,41 +1,41 @@
 describe("password strength functions", function(){
     var scope, controller;
-    
     beforeEach(function(){
-        angular.module("signupForm");
+        angular.mock.module("validationApp");
     });
     beforeEach(inject(function($controller, $rootScope){
         scope = $rootScope.$new();
         controller = $controller("signupForm", {$scope: scope});
+        scope.signup = {};
     }));
     function checkStrength(password, toCheck, expected){
-        $scope.signup.password = password;
-        $scope.getStrength();
-        expect(toCheck).toEqual(expected);
+        scope.signup.password = password;
+        scope.getStrength();
+        expect(scope[toCheck]).toEqual(expected);
     }
     it("should have capped strength when there is no variety", function(){
-        checkStrength("thisisreallylongpasswordwithnovariety", $scope.passwordStrength, "25");
-        checkStrength("thisisreallylongpasswordwithnovarietyA", $scope.passwordStrength, "50");
-        checkStrength("thisisreallylongpasswordwithnovarietyA1", $scope.passwordStrength, "75");
-        checkStrength("thisisreallylongpasswordwithnovarietyA1!", $scope.passwordStrength, "100");
+        checkStrength("thisisreallylongpasswordwithnovariety", "passwordStrength", "25");
+        checkStrength("thisisreallylongpasswordwithnovarietyA", "passwordStrength", "50");
+        checkStrength("thisisreallylongpasswordwithnovarietyA1", "passwordStrength", "75");
+        checkStrength("thisisreallylongpasswordwithnovarietyA1!", "passwordStrength", "100");
     });
     it("should increment by multiplier",function(){
         /* maybe should be a global on scope, but don't like that */
         var multiplier = 8;
-        checkStrength("a", $scope.passwordStrength, (multiplier).toString());
-        checkStrength("ab", $scope.passwordStrength, (multiplier*2).toString());
-        checkStrength("abc", $scope.passwordStrength, (multiplier*3).toString());
+        checkStrength("a", "passwordStrength", (multiplier).toString());
+        checkStrength("ab", "passwordStrength", (multiplier*2).toString());
+        checkStrength("abc", "passwordStrength", (multiplier*3).toString());
     });
     it("should give proper strength words", function(){
-        checkStrength("thisisreallylongpasswordwithnovariety", $scope.strengthWord, "Weak");
-        checkStrength("thisisreallylongpasswordwithnovarietyA", $scope.strengthWord, "Okay");
-        checkStrength("thisisreallylongpasswordwithnovarietyA1", $scope.strengthWord, "Strong");
-        checkStrength("thisisreallylongpasswordwithnovarietyA1!", $scope.strengthWord, "Really Strong!");
+        checkStrength("thisisreallylongpasswordwithnovariety", "strengthWord", "Weak");
+        checkStrength("thisisreallylongpasswordwithnovarietyA", "strengthWord", "Okay");
+        checkStrength("thisisreallylongpasswordwithnovarietyA1", "strengthWord", "Strong");
+        checkStrength("thisisreallylongpasswordwithnovarietyA1!", "strengthWord", "Really Strong!");
     });
     it("should give proper progress bar classes", function(){
-        checkStrength("thisisreallylongpasswordwithnovariety", $scope.strengthClass, "progress-bar-danger");
-        checkStrength("thisisreallylongpasswordwithnovarietyA", $scope.strengthClass, "progress-bar-warning");
-        checkStrength("thisisreallylongpasswordwithnovarietyA1", $scope.strengthClass, "progress-bar-info");
-        checkStrength("thisisreallylongpasswordwithnovarietyA1!", $scope.strengthClass, "progress-bar-success");
+        checkStrength("thisisreallylongpasswordwithnovariety", "strengthClass", "progress-bar-danger");
+        checkStrength("thisisreallylongpasswordwithnovarietyA", "strengthClass", "progress-bar-warning");
+        checkStrength("thisisreallylongpasswordwithnovarietyA1", "strengthClass", "progress-bar-info");
+        checkStrength("thisisreallylongpasswordwithnovarietyA1!", "strengthClass", "progress-bar-success");
     });
 });
